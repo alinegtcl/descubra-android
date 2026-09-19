2a1ef2397e6bbe660bd2c8dc50a60cb9b0dbd039
Aline Tolentino <linegcruz@gmail.com>
2026-09-19 13:57:19 -0300

    test: adiciona testes unitários à DescubraViewModel.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
     implementation(libs.kotlinx.coroutines.android)
 
     testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.coroutines.test)
 
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
