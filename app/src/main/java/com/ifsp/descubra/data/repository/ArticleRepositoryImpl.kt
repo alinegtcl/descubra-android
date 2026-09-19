b946d25e3511e544fc7bd0e6f7347d6af244b385
Aline Tolentino <linegcruz@gmail.com>
2026-09-19 12:58:31 -0300

    feat: insere camada de dominio e Article como domain entity.

--- app/src/main/java/com/ifsp/descubra/data/repository/ArticleRepositoryImpl.kt
+++ app/src/main/java/com/ifsp/descubra/data/repository/ArticleRepositoryImpl.kt
@@ -1,13 +1,17 @@
 package com.ifsp.descubra.data.repository
 
-import com.ifsp.descubra.data.remote.WikipediaResponse
 import com.ifsp.descubra.data.remote.WikipediaService
+import com.ifsp.descubra.domain.model.Article
 
 class ArticleRepositoryImpl(
     private val wikipediaService: WikipediaService
 ) : ArticleRepository {
 
-    override suspend fun getRandomArticle(): WikipediaResponse {
-        return wikipediaService.getRandomArticle()
+    override suspend fun getRandomArticle(): Article {
+        val response = wikipediaService.getRandomArticle()
+        return Article(
+            title = response.title,
+            description = response.extract
+        )
     }
 }
---
+++ app/src/main/java/com/ifsp/descubra/domain/model/Article.kt
@@ -0,0 +1,6 @@
+package com.ifsp.descubra.domain.model
+
+data class Article(
+    val title: String?,
+    val description: String?
+)
