429e64195fe10e2842d86fdc94d149a9510db5fa
Aline Tolentino <linegcruz@gmail.com>
2026-09-19 11:51:00 -0300

    fix: altera inicialização de DescubraViewModel

--- app/src/main/java/com/ifsp/descubra/MainActivity.kt
+++ app/src/main/java/com/ifsp/descubra/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ifsp.descubra.ui.theme.DescubraTheme
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel = DescubraViewModel()
+    private val viewModel : DescubraViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
